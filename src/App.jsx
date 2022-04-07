af59ab2c0618f9e14f0d12cfd0e559a0abe197a4
Arjun Kalburgi <askalburgi@gmail.com>
2022-04-07 18:31:37 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import '@nylas/components-agenda';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="teams">
+        <section>
+          <h2>Dorothy</h2>
+<nylas-agenda id="6484c482-4eea-45fb-bd74-331756839f27" > </nylas-agenda>
+        </section>
+        <section>
+          <h2>Katherine</h2>
+<nylas-agenda id="a29bcb9d-c49b-418b-83fa-b6b01130c599" > </nylas-agenda>
+        </section>
+        <section>
+          <h2>Mae</h2>
+<nylas-agenda id="396be006-d249-4117-90c4-8ee382c9fda0" > </nylas-agenda>
+        </section>
+      </div>
+    </div>
+  );
+}
+
+export default App;
